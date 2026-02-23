f097ee08c1ec5f4011eff706137b975590947cec
SE-Kcm <147773307+SE-Kcm@users.noreply.github.com>
2026-02-23 03:03:23 +0300

    Handle invalid input and non-existent IDs in TaskController with ResponseEntity

--- src/main/java/com/taskManager/spring/TaskController.java
+++ src/main/java/com/taskManager/spring/TaskController.java
@@ -3,6 +3,7 @@ package com.taskManager.spring;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,23 +19,45 @@ public class TaskController{
         this.taskService = taskService;
     }
     @RequestMapping(value="/tasks", method=RequestMethod.POST)
-    public void addTask(@RequestBody Map<String, String> body){
-        taskService.addTask(body.get("name"));
+    public ResponseEntity<Void> addTask(@RequestBody Map<String, String> body){
+        String taskName = body.get("name");
+        if(!taskName.isEmpty() && taskName != null){
+            taskService.addTask(taskName);
+            return ResponseEntity.ok().build(); //need build because there is no response body (build: Build the response entity with no body.)
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @RequestMapping(value="/tasks", method=RequestMethod.GET)
-    public List<Task> listAllTasks(){
-        return taskService.getAllTasks(); 
+    public ResponseEntity<List<Task>> listAllTasks(){
+        List<Task> resList =  taskService.getAllTasks(); 
+        if(resList.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.ok(resList);
+        }
+        
     }
 
     @RequestMapping(value="/tasks/{id}/done", method=RequestMethod.PUT)
-    public void markTask(@PathVariable int id){
-        taskService.markTask(id);
+    public ResponseEntity<Void> markTask(@PathVariable int id){
+        boolean resVal = taskService.markTask(id);
+        if(resVal){
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @RequestMapping(value="/tasks/{id}", method=RequestMethod.DELETE)
-    public void deleteTask(@PathVariable int id){
-        taskService.deleteTask(id);
+    public ResponseEntity<Void> deleteTask(@PathVariable int id){
+        boolean resVal = taskService.deleteTask(id);
+        if(resVal){
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
